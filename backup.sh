052948d2a21114abe1fe0ed44a6b41c5f58c04a6
Connor Moody <mcm1053@protonmail.com>
2022-04-18 12:15:19 -0500

    First commit

---
+++ backup.sh
@@ -0,0 +1,30 @@
+# https://linuxconfig.org/how-to-create-incremental-backups-using-rsync-on-linux
+# A script to perform incremental backups using rsync
+# NEVER RUN WITH SUDO
+#!/bin/bash
+
+# Failsafe, stops the script if errors occur
+set -Eeuo pipefail
+
+readonly SOURCE_DIR="${HOME}"
+readonly BACKUP_DIR="/run/media/connor/backup/"
+readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
+readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
+readonly LATEST_LINK="${BACKUP_DIR}/latest"
+
+mkdir -p "${BACKUP_DIR}"
+
+rsync -av --delete \
+  "${SOURCE_DIR}/" \
+  --link-dest "${LATEST_LINK}" \
+  --exclude=".cache" \
+  --exclude=".local" \
+  --exclude=".mozilla" \
+  --exclude="Documents" \
+  "${BACKUP_PATH}"
+
+rm -rf "${LATEST_LINK}"
+ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
+
+# Backup list of packages
+pacman -Qe > /run/media/connor/backup/packages.txt
