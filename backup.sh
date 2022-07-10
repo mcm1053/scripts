ecdba5f8bd9b0faf1d68de033986d2f069b07366
Connor Moody <mcm1053@protonmail.com>
2022-07-10 11:52:50 -0500

    updated script

--- backup.sh
+++ backup.sh
@@ -1,13 +1,12 @@
+# Incremental backup script
 # https://linuxconfig.org/how-to-create-incremental-backups-using-rsync-on-linux
-# A script to perform incremental backups using rsync
-# NEVER RUN WITH SUDO
 #!/bin/bash
 
-# Failsafe, stops the script if errors occur
-set -Eeuo pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
-readonly SOURCE_DIR="${HOME}"
-readonly BACKUP_DIR="/run/media/connor/backup/"
+readonly BACKUP_DIR="/home/connor/MEGA/archive/backup"
 readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
 readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
 readonly LATEST_LINK="${BACKUP_DIR}/latest"
@@ -15,16 +14,14 @@ readonly LATEST_LINK="${BACKUP_DIR}/latest"
 mkdir -p "${BACKUP_DIR}"
 
 rsync -av --delete \
-  "${SOURCE_DIR}/" \
+  /home/connor/.config \
   --link-dest "${LATEST_LINK}" \
-  --exclude=".cache" \
-  --exclude=".local" \
-  --exclude=".mozilla" \
-  --exclude="Documents" \
   "${BACKUP_PATH}"
+rsync -av --delete \
+  /home/connor/.bashrc \
+  --link-dest "${LATEST_LINK}" \
+  "${BACKUP_PATH}"
+pacman -Qe > "${BACKUP_PATH}/packages.txt"
 
 rm -rf "${LATEST_LINK}"
 ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
-
-# Backup list of packages
-pacman -Qe > /run/media/connor/backup/packages.txt
